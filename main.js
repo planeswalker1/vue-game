8fc50a1544d5c36b24a1c74ed78e72f9ffb37961
Daniel Munoz <daniel.munozdev@gmail.com>
2018-09-18 16:24:04 -0700

    add game functionality and css

--- main.js
+++ main.js
@@ -2,7 +2,32 @@
 const audio = new Audio('sounds/Roblox-death-sound-effect.mp3');
 audio.load();
 
-// vue instance
+// container vue instance
 new Vue({
-  el: '.container'
+  el: '.container',
+  data: {
+    health: 100,
+    ended: false
+  },
+  methods: {
+    punch: function () {
+      this.health -= 10;
+      if (this.health <= 0) {
+        this.ended = true;
+        audio.play();
+      }
+    },
+    restart: function () {
+      this.health = 100;
+      this.ended = false;
+    }
+  },
+  computed: {
+    headClasses: function () {
+      return {
+        head: true,
+        rip: this.ended
+      };
+    }
+  }
 });
